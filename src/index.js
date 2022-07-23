389d069a54014c24cca78ce667fade60204db219
jiyun <sprare06@gmail.com>
2022-07-23 10:10:45 +0900

    store데이터 App컴포넌트에 전달

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { HashRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import youtubeReducer from "./redux/youtubeSlice";
+
+const store = configureStore({
+  reducer: {
+    youtube: youtubeReducer,
+  },
+});
 
 ReactDOM.render(
-	<HashRouter>
-		<App />
-	</HashRouter>,
-	document.getElementById('root')
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>,
+  document.getElementById("root")
 );
