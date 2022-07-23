d6a93eab4ff84bfdae3693f6cb9a777691807a2b
jiyun <sprare06@gmail.com>
2022-07-23 10:30:37 +0900

    store 전역 데이터 원하는 컴포넌트에서 useSelector로 가져오기

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,7 +1,16 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 function Youtube() {
-  return <div>Youtube</div>;
+  const youtube = useSelector((store) => store.youtube.data);
+
+  return (
+    <section>
+      <h1>Youtube</h1>
+      {youtube.map((vid) => {
+        return <p key={vid.id}>{vid.snippet.title}</p>;
+      })}
+    </section>
+  );
 }
 
 export default Youtube;
