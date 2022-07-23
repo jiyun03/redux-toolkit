b949e1fc54367c2e3a9e7abb081505ed45cc004d
jiyun <sprare06@gmail.com>
2022-07-23 11:32:38 +0900

    메인컴포넌트에 flickr 미리보기 출력

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 
 function Main() {
   const youtube = useSelector((store) => store.youtube.data);
+  const flickr = useSelector((store) => store.flickr.data);
 
   return (
     <section>
@@ -11,6 +12,23 @@ function Main() {
           {youtube.map((vid, idx) => {
             if (idx >= 3) return;
             return <li key={vid.id}>{vid.snippet.title}</li>;
+          })}
+        </ul>
+      </article>
+      <article>
+        <h2>Flickr</h2>
+        <ul>
+          {flickr.map((pic, idx) => {
+            if (idx >= 3) return;
+            return (
+              <li key={pic.id}>
+                <img
+                  src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+                  alt={pic.title}
+                />
+                {pic.owner}
+              </li>
+            );
           })}
         </ul>
       </article>
